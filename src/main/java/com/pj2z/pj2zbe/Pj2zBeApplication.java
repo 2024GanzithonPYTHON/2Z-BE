32da927b52a652519c44a8123c699754c868a3dc
shinheekim <shinhee1241@gmail.com>
2024-11-17 20:52:39 +0900

    init commit

---
+++ src/main/java/com/pj2z/pj2zbe/Pj2zBeApplication.java
@@ -0,0 +1,13 @@
+package com.pj2z.pj2zbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pj2zBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pj2zBeApplication.class, args);
+	}
+
+}
