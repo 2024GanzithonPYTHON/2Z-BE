358a7c668ca498e8f9adfee1ef9ae733faf1e6ca
Yerim <yerim0307@gmail.com>
2024-11-23 09:59:04 +0900

    fix: test column min, max 추가

--- src/main/java/com/pj2z/pj2zbe/test/repository/TestRepository.java
+++ src/main/java/com/pj2z/pj2zbe/test/repository/TestRepository.java
@@ -5,7 +5,10 @@ import com.pj2z.pj2zbe.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TestRepository extends JpaRepository<Test, Long> {
+    Optional<Test> findByUserId(Long aLong);
     Test findTopByUserOrderByCreatedAtDesc(UserEntity user);
 }
